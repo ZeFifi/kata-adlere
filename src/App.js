95ec646dc979ccdc4ad93b006e615c9b50b365c1
ZeFifi <ph.pinceloup@protonmail.com>
2019-07-22 12:41:52 +0200

    Created the Admin component and route

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Admin from "./components/Admin/Admin";
 
 function App() {
   return (
       <Router>
               <Route exact path="/" component={Home} />
+              <Route path="/admin" component={Admin} />
       </Router>
   );
 }
---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Admin extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Welcome on the admin page</h1>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Admin;
