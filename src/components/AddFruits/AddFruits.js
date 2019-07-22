9f697699262387bc02fec876aca6e56bfaa066bb
ZeFifi <ph.pinceloup@protonmail.com>
2019-07-22 13:16:59 +0200

    Can post to the API successfully

---
+++ src/components/AddFruits/AddFruits.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import axios from 'axios';
+
+class AddFruits extends React.Component {
+    state = {
+        name: '',
+        description: '',
+        price: '',
+        image: '',
+    };
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const { name, description, price, image} = this.state;
+
+        axios.post(`http://localhost:3001/fruits`, { name, description, price, image})
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+    };
+
+    render() {
+        const { name, description, price, image} = this.state;
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Fruit name:
+                        <input type="text" name="name" value={name} onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        Description:
+                        <input type="text" name="description" value={description} onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        Price:
+                        <input type="text" name="price" value={price} onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        Image:
+                        <input type="text" name="image" value={image} onChange={this.handleChange} />
+                    </label>
+                    <button type="submit">Add</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddFruits
--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import AddFruits from "../AddFruits/AddFruits";
 
 class Admin extends Component {
     render() {
         return (
             <React.Fragment>
                 <h1>Welcome on the admin page</h1>
+                <AddFruits/>
             </React.Fragment>
         );
     }
