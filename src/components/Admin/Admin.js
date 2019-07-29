ef072b023ffafa41c5de12aef9674f96ec7c60b2
ZeFifi <ph.pinceloup@protonmail.com>
2019-07-29 11:58:41 +0200

    Admin is a functionnal component now

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import AddItems from "../AddItems/AddItems";
 import ProductsList from "../ProductsList/ProductsList";
 
-class Admin extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div className="container">
-          <h1 className="mb-5">Bienvenue sur la page d'administration</h1>
-          <AddItems />
-          <h2>Produits mis en ligne</h2>
-          <ProductsList />
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+const Admin = () => {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <h1 className="mb-5">Bienvenue sur la page d'administration</h1>
+        <AddItems/>
+        <h2>Produits mis en ligne</h2>
+        <ProductsList/>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Admin;
